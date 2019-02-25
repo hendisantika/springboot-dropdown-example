e26c5d478c2da4dd17da94a365ca5875b75d35ba
hendisantika <hendisantika@yahoo.co.id>
2019-02-26 06:20:14 +0700

    #1 - Initial commit

---
+++ src/main/java/com/hendisantika/springbootdropdownexample/SpringbootDropdownExampleApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootdropdownexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDropdownExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDropdownExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/springbootdropdownexample/SpringbootDropdownExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hendisantika.springbootdropdownexample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootDropdownExampleApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
