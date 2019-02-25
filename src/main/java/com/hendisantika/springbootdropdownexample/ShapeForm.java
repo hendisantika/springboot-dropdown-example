86be1ca891304c7bc7e8f3a52643182d9abfefd4
hendisantika <hendisantika@yahoo.co.id>
2019-02-26 06:23:46 +0700

    #3 - Add Shape Form & lombok lib

---
+++ src/main/java/com/hendisantika/springbootdropdownexample/ShapeForm.java
@@ -0,0 +1,18 @@
+package com.hendisantika.springbootdropdownexample;
+
+import lombok.Data;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-dropdown-example
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-02-26
+ * Time: 06:22
+ */
+@Data
+public class ShapeForm {
+    private String shapeType;
+    private String shape;
+}
